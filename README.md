30ec1805fd30e78d2453e916e901d0bc2f3b1025
leo <leo@leodeMacBook-Pro.local>
2021-10-13 17:29:52 +0800

    update

--- README.md
+++ README.md
@@ -6,55 +6,53 @@ go get "github.com/daqnext/LocalLog/log"
 ```
 
 ```go
- package main
+package main
 
 import (
 	"fmt"
-
 	"github.com/daqnext/LocalLog/log"
 )
 
 /////////////////////
 
-func init() {
-	//logsAbsFolder_, fileMaxSizeMBytes, MaxBackupsFiles, MaxAgeDays, loglevel
-	log.Initialize("logs", 2, 20, 30, log.LEVEL_DEBUG)
-}
-
 func main() {
 
-	log.WithFields(log.Fields{
+	llog, err := log.New("logs", 2, 20, 30, log.LEVEL_DEBUG)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	llog.WithFields(log.Fields{
 		"f1": "1",
 		"f2": "2",
 	}).Error("Total Error Fileds : %d", 2)
 
-	log.WithFields(log.Fields{
+	llog.WithFields(log.Fields{
 		"f1": "1",
 		"f2": "2",
 	}).Warn("Total Warn Fileds : %d", 2)
 
-	log.WithFields(log.Fields{
+	llog.WithFields(log.Fields{
 		"f1": "1",
 		"f2": "2",
 	}).Info("Total Fileds : %d", 2)
 
-	log.WithFields(log.Fields{
+	llog.WithFields(log.Fields{
 		"f1": "1",
 		"f2": "2",
 	}).Debug("Total Debug Fileds : %d", 2)
 
 	fmt.Println("////////////////////////////////////////////////////////////////////////////////")
 	//all logs include all types :debug ,info ,warning ,error,panic ,fatal
-	log.PrintLastN_AllLogs(100)
+	llog.PrintLastN_AllLogs(100)
 	fmt.Println("////////////////////////////////////////////////////////////////////////////////")
 	//err logs include all types :,error,panic ,fatal
-	log.PrintLastN_ErrLogs(100)
+	llog.PrintLastN_ErrLogs(100)
 	fmt.Println("////////////////////////////////////////////////////////////////////////////////")
 
-	log.Warnln("this is warn ln")
-	log.Warnf("this is warnf %d", 123)
+	llog.Warnln("this is warn ln")
+	llog.Warnf("this is warnf %d", 123)
 
 }
-
 
 ```
--- log/log.go
+++ log/log.go
@@ -13,8 +13,8 @@ import (
 	"github.com/sirupsen/logrus"
 )
 
-const LEVEL_FATAL = "FATA"
 const LEVEL_PANIC = "PANI"
+const LEVEL_FATAL = "FATA"
 const LEVEL_ERROR = "ERRO"
 const LEVEL_WARN = "WARN"
 const LEVEL_INFO = "INFO"
@@ -24,9 +24,10 @@ var logsAbsFolder string
 var logsAllAbsFolder string
 var logsErrorAbsFolder string
 
-type Fields map[string]interface{}
+type Fields = logrus.Fields
 
 func checkAndMkDir(logsAbsFolder string) error {
+
 	info, err := os.Stat(logsAbsFolder)
 	if err != nil {
 		if os.IsNotExist(err) {
@@ -53,7 +54,13 @@ func checkAndMkDir(logsAbsFolder string) error {
 	}
 }
 
-func Initialize(logsAbsFolder_ string, fileMaxSizeMBytes int, MaxBackupsFiles int, MaxAgeDays int, loglevel string) error {
+type LocalLog struct {
+	logrus.Logger
+	ALL_LogfolderABS string
+	ERR_LogfolderABS string
+}
+
+func New(logsAbsFolder_ string, fileMaxSizeMBytes int, MaxBackupsFiles int, MaxAgeDays int, loglevel string) (*LocalLog, error) {
 
 	var LLevel logrus.Level
 
@@ -72,6 +79,8 @@ func Initialize(logsAbsFolder_ string, fileMaxSizeMBytes int, MaxBackupsFiles in
 		LLevel = logrus.DebugLevel
 	}
 
+	logger := logrus.New()
+
 	logsAbsFolder = strings.Trim(logsAbsFolder_, "/")
 	logsAllAbsFolder = logsAbsFolder + "/all"
 	logsErrorAbsFolder = logsAbsFolder + "/error"
@@ -79,15 +88,15 @@ func Initialize(logsAbsFolder_ string, fileMaxSizeMBytes int, MaxBackupsFiles in
 	//make sure the logs folder exist otherwise create dir
 	dirError := checkAndMkDir(logsAbsFolder)
 	if dirError != nil {
-		return dirError
+		return nil, dirError
 	}
 	dirError = checkAndMkDir(logsAllAbsFolder)
 	if dirError != nil {
-		return dirError
+		return nil, dirError
 	}
 	dirError = checkAndMkDir(logsErrorAbsFolder)
 	if dirError != nil {
-		return dirError
+		return nil, dirError
 	}
 	///////////////////////
 
@@ -107,7 +116,7 @@ func Initialize(logsAbsFolder_ string, fileMaxSizeMBytes int, MaxBackupsFiles in
 		}},
 	})
 	if err_all != nil {
-		return err_all
+		return nil, err_all
 	}
 
 	rotateFileHook_ERR, err_err := NewRotateFileHook(RotateFileConfig{
@@ -124,27 +133,23 @@ func Initialize(logsAbsFolder_ string, fileMaxSizeMBytes int, MaxBackupsFiles in
 	})
 
 	if err_err != nil {
-		return err_err
+		return nil, err_err
 	}
 
-	logrus.SetFormatter(UTCFormatter{&nested.Formatter{
+	logger.SetFormatter(UTCFormatter{&nested.Formatter{
 		HideKeys:        false,
 		TimestampFormat: "2006-01-02 15:04:05",
 	}})
 
-	logrus.SetLevel(LLevel)
-	logrus.AddHook(rotateFileHook_ALL)
-	logrus.AddHook(rotateFileHook_ERR)
+	logger.SetLevel(LLevel)
+	logger.AddHook(rotateFileHook_ALL)
+	logger.AddHook(rotateFileHook_ERR)
 
-	return nil
+	return &LocalLog{*logger, alllogfile, errlogfile}, nil
 
 }
 
-func WithFields(keymap map[string]interface{}) *logrus.Entry {
-	return logrus.WithFields(logrus.Fields(keymap))
-}
-
-func getLogFilesList(log_folder string) ([]string, error) {
+func (logger *LocalLog) GetLogFilesList(log_folder string) ([]string, error) {
 
 	var result []string
 	files, err := ioutil.ReadDir(log_folder)
@@ -158,25 +163,25 @@ func getLogFilesList(log_folder string) ([]string, error) {
 	return result, nil
 }
 
-func PrintLastN_ErrLogs(lastN int) {
-	printLastNLogs("error", lastN)
+func (logger *LocalLog) PrintLastN_ErrLogs(lastN int) {
+	logger.printLastNLogs("error", lastN)
 }
 
-func PrintLastN_AllLogs(lastN int) {
-	printLastNLogs("all", lastN)
+func (logger *LocalLog) PrintLastN_AllLogs(lastN int) {
+	logger.printLastNLogs("all", lastN)
 }
 
-func printLastNLogs(type_ string, lastN int) {
+func (logger *LocalLog) printLastNLogs(type_ string, lastN int) {
 
 	var alllogfiles []string
 	var err error
 	var folder string
 	if type_ == "error" {
-		folder = logsErrorAbsFolder
+		folder = logger.ERR_LogfolderABS
 	} else {
-		folder = logsAllAbsFolder
+		folder = logger.ALL_LogfolderABS
 	}
-	alllogfiles, err = getLogFilesList(folder)
+	alllogfiles, err = logger.GetLogFilesList(folder)
 
 	if err != nil {
 		fmt.Println(string(Red), err.Error())
@@ -235,85 +240,4 @@ func splitLines(s string) []string {
 		lines = append(lines, sc.Text())
 	}
 	return lines
-
-}
-
-// Trace logs a message at level Trace on the standard logger.
-func Traceln(args ...interface{}) {
-	logrus.Traceln(args...)
-}
-
-// Debug logs a message at level Debug on the standard logger.
-func Debugln(args ...interface{}) {
-	logrus.Debugln(args...)
-}
-
-// Print logs a message at level Info on the standard logger.
-func Println(args ...interface{}) {
-	logrus.Println(args...)
-}
-
-// Info logs a message at level Info on the standard logger.
-func Infoln(args ...interface{}) {
-	logrus.Infoln(args...)
-}
-
-// Warn logs a message at level Warn on the standard logger.
-func Warnln(args ...interface{}) {
-	logrus.Warnln(args...)
-}
-
-// Error logs a message at level Error on the standard logger.
-func Errorln(args ...interface{}) {
-	logrus.Errorln(args...)
-}
-
-// Panic logs a message at level Panic on the standard logger.
-func Panicln(args ...interface{}) {
-	logrus.Panicln(args...)
-}
-
-// Fatal logs a message at level Fatal on the standard logger then the process will exit with status set to 1.
-func Fatalln(args ...interface{}) {
-	logrus.Fatalln(args...)
-}
-
-// Tracef logs a message at level Trace on the standard logger.
-func Tracef(format string, args ...interface{}) {
-	logrus.Tracef(format, args...)
-}
-
-// Debugf logs a message at level Debug on the standard logger.
-func Debugf(format string, args ...interface{}) {
-	logrus.Debugf(format, args...)
-}
-
-// Printf logs a message at level Info on the standard logger.
-func Printf(format string, args ...interface{}) {
-	logrus.Printf(format, args...)
-}
-
-// Infof logs a message at level Info on the standard logger.
-func Infof(format string, args ...interface{}) {
-	logrus.Infof(format, args...)
-}
-
-// Warnf logs a message at level Warn on the standard logger.
-func Warnf(format string, args ...interface{}) {
-	logrus.Warnf(format, args...)
-}
-
-// Errorf logs a message at level Error on the standard logger.
-func Errorf(format string, args ...interface{}) {
-	logrus.Errorf(format, args...)
-}
-
-// Panicf logs a message at level Panic on the standard logger.
-func Panicf(format string, args ...interface{}) {
-	logrus.Panicf(format, args...)
-}
-
-// Fatalf logs a message at level Fatal on the standard logger then the process will exit with status set to 1.
-func Fatalf(format string, args ...interface{}) {
-	logrus.Fatalf(format, args...)
 }
