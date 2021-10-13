ebb3c8c636ce7c6b490aa613ae34dbcd34fd8a8c
leo <leo@leodeMacBook-Pro.local>
2021-10-13 10:05:58 +0800

    update

--- log/log.go
+++ log/log.go
@@ -235,4 +235,85 @@ func splitLines(s string) []string {
 		lines = append(lines, sc.Text())
 	}
 	return lines
+
+}
+
+// Trace logs a message at level Trace on the standard logger.
+func Traceln(args ...interface{}) {
+	logrus.Traceln(args...)
+}
+
+// Debug logs a message at level Debug on the standard logger.
+func Debugln(args ...interface{}) {
+	logrus.Debugln(args...)
+}
+
+// Print logs a message at level Info on the standard logger.
+func Println(args ...interface{}) {
+	logrus.Println(args...)
+}
+
+// Info logs a message at level Info on the standard logger.
+func Infoln(args ...interface{}) {
+	logrus.Infoln(args...)
+}
+
+// Warn logs a message at level Warn on the standard logger.
+func Warnln(args ...interface{}) {
+	logrus.Warnln(args...)
+}
+
+// Error logs a message at level Error on the standard logger.
+func Errorln(args ...interface{}) {
+	logrus.Errorln(args...)
+}
+
+// Panic logs a message at level Panic on the standard logger.
+func Panicln(args ...interface{}) {
+	logrus.Panicln(args...)
+}
+
+// Fatal logs a message at level Fatal on the standard logger then the process will exit with status set to 1.
+func Fatalln(args ...interface{}) {
+	logrus.Fatalln(args...)
+}
+
+// Tracef logs a message at level Trace on the standard logger.
+func Tracef(format string, args ...interface{}) {
+	logrus.Tracef(format, args...)
+}
+
+// Debugf logs a message at level Debug on the standard logger.
+func Debugf(format string, args ...interface{}) {
+	logrus.Debugf(format, args...)
+}
+
+// Printf logs a message at level Info on the standard logger.
+func Printf(format string, args ...interface{}) {
+	logrus.Printf(format, args...)
+}
+
+// Infof logs a message at level Info on the standard logger.
+func Infof(format string, args ...interface{}) {
+	logrus.Infof(format, args...)
+}
+
+// Warnf logs a message at level Warn on the standard logger.
+func Warnf(format string, args ...interface{}) {
+	logrus.Warnf(format, args...)
+}
+
+// Errorf logs a message at level Error on the standard logger.
+func Errorf(format string, args ...interface{}) {
+	logrus.Errorf(format, args...)
+}
+
+// Panicf logs a message at level Panic on the standard logger.
+func Panicf(format string, args ...interface{}) {
+	logrus.Panicf(format, args...)
+}
+
+// Fatalf logs a message at level Fatal on the standard logger then the process will exit with status set to 1.
+func Fatalf(format string, args ...interface{}) {
+	logrus.Fatalf(format, args...)
 }
