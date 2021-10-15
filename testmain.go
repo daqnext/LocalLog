a6af65be8a15d5884afb97376c72794afc3f84d1
leo <leo@leodeMacBook-Pro.local>
2021-10-15 08:54:08 +0800

    update

--- testmain.go
+++ testmain.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"os"
 	"os/exec"
 	"path/filepath"
@@ -40,46 +39,48 @@ func main() {
 		panic(err.Error())
 	}
 
-	llog.WithFields(log.Fields{
-		"f1": "1",
-		"f2": "2",
-	}).Error("Total xxx Error Fileds : %d", 2)
-
-	llog.WithFields(log.Fields{
-		"f1": "1",
-		"f2": "2",
-	}).Warn("Total  yy Warn Fileds : %d", 2)
-
-	llog.ResetLevel(log.LEVEL_DEBUG)
-
-	llog.WithFields(log.Fields{
-		"f1": "1",
-		"f2": "2",
-	}).Info("Total zzz Fileds : %d", 2)
-
-	llog.WithFields(log.Fields{
-		"f1": "1",
-		"f2": "2",
-	}).Debug("Total Debug Fileds : %d", 2)
-
-	llog.ResetLevel(log.LEVEL_TRACE)
-
-	llog.WithFields(log.Fields{
-		"f1": "1",
-		"f2": "2",
-	}).Debug("Total Debug Fileds : %d", 2)
-
-	fmt.Println("////////////////////////////////////////////////////////////////////////////////")
-	//all logs include all types :debug ,info ,warning ,error,panic ,fatal
 	llog.PrintLastN_AllLogs(100)
-	fmt.Println("////////////////////////////////////////////////////////////////////////////////")
-	//err logs include all types :,error,panic ,fatal
-	llog.PrintLastN_ErrLogs(100)
-	fmt.Println("////////////////////////////////////////////////////////////////////////////////")
 
-	llog.Warnln("this is warn ln")
-	llog.Warnf("this is warnf %d", 123)
+	// llog.WithFields(log.Fields{
+	// 	"f1": "1",
+	// 	"f2": "2",
+	// }).Error("Total xxx Error Fileds : %d", 2)
 
-	llog.Traceln("this is warn ln")
+	// llog.WithFields(log.Fields{
+	// 	"f1": "1",
+	// 	"f2": "2",
+	// }).Warn("Total  yy Warn Fileds : %d", 2)
+
+	// llog.ResetLevel(log.LEVEL_DEBUG)
+
+	// llog.WithFields(log.Fields{
+	// 	"f1": "1",
+	// 	"f2": "2",
+	// }).Info("Total zzz Fileds : %d", 2)
+
+	// llog.WithFields(log.Fields{
+	// 	"f1": "1",
+	// 	"f2": "2",
+	// }).Debug("Total Debug Fileds : %d", 2)
+
+	// llog.ResetLevel(log.LEVEL_TRACE)
+
+	// llog.WithFields(log.Fields{
+	// 	"f1": "1",
+	// 	"f2": "2",
+	// }).Debug("Total Debug Fileds : %d", 2)
+
+	// fmt.Println("////////////////////////////////////////////////////////////////////////////////")
+	// //all logs include all types :debug ,info ,warning ,error,panic ,fatal
+	// llog.PrintLastN_AllLogs(100)
+	// fmt.Println("////////////////////////////////////////////////////////////////////////////////")
+	// //err logs include all types :,error,panic ,fatal
+	// llog.PrintLastN_ErrLogs(100)
+	// fmt.Println("////////////////////////////////////////////////////////////////////////////////")
+
+	// llog.Warnln("this is warn ln")
+	// llog.Warnf("this is warnf %d", 123)
+
+	// llog.Traceln("this is warn ln")
 
 }
